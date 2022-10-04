d85246623e7fb0172880ef47c4386154a4c3ba58
Song Zhou <zs@ziorix.com>
2022-10-04 22:09:02 +0800

    decryption handler

---
+++ packages/request-handler/src/decryptMessage.spec.ts
@@ -0,0 +1,146 @@
+// Copyright 2021-2022 @choko-wallet/request-handler authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { KeypairType } from '@choko-wallet/core/types';
+
+import { cryptoWaitReady, mnemonicToMiniSecret } from '@polkadot/util-crypto';
+import { AsymmetricEncryption } from '@skyekiwi/crypto';
+import { randomBytes } from 'tweetnacl';
+
+import { AccountOption, RequestError, UserAccount } from '@choko-wallet/core';
+import { DappDescriptor } from '@choko-wallet/core/dapp';
+import { knownNetworks } from '@choko-wallet/known-networks';
+
+import { DecryptMessageDescriptor, DecryptMessageRequest, DecryptMessageRequestPayload, DecryptMessageResponse, DecryptMessageResponsePayload } from './decryptMessage';
+
+const SEED = 'leg satisfy enlist dizzy rib owner security live solution panther monitor replace';
+
+describe('@choko-wallet/request-handler - decryptMessage', function () {
+  beforeAll(async () => {
+    await cryptoWaitReady();
+  });
+
+  ['sr25519', 'ed25519'].map((keyType) => {
+    const account = new UserAccount(new AccountOption({
+      hasEncryptedPrivateKeyExported: false,
+      keyType: keyType as KeypairType,
+      localKeyEncryptionStrategy: 0
+    }));
+    const dapp = new DappDescriptor({
+      activeNetwork: knownNetworks['847e7b7fa160d85f'], // skyekiwi
+      displayName: 'Jest Testing',
+      infoName: 'test',
+      version: 0
+    });
+
+    it(`request serde - decryptMessage ${keyType}`, async () => {
+      const msg = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+      account.unlock(mnemonicToMiniSecret(SEED));
+      await account.init();
+      account.lock();
+
+      const request = new DecryptMessageRequest({
+        dappOrigin: dapp,
+        payload: new DecryptMessageRequestPayload({
+          keyType: keyType as KeypairType,
+          message: msg,
+          receiptPublicKey: new Uint8Array(32)
+        }),
+        userOrigin: account
+      });
+
+      const serialized = request.serialize();
+
+      const deserialized = DecryptMessageRequest.deserialize(serialized);
+
+      expect(deserialized.payload.message).toEqual(msg);
+      expect(deserialized.payload.receiptPublicKey).toEqual(new Uint8Array(32));
+      expect(deserialized.payload.keyType).toEqual(keyType);
+
+      expect(deserialized.dappOrigin).toEqual(dapp);
+      expect(deserialized.userOrigin).toEqual(account);
+    });
+
+    it(`response serde - decryptMessage ${keyType}`, async () => {
+      account.unlock(mnemonicToMiniSecret(SEED));
+      await account.init();
+      account.lock();
+      const msg = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+      const response = new DecryptMessageResponse({
+        dappOrigin: dapp,
+        payload: new DecryptMessageResponsePayload({
+          message: msg
+        }),
+        userOrigin: account
+      });
+
+      const serialized = response.serialize();
+      const deserialized = DecryptMessageResponse.deserialize(serialized);
+
+      expect(deserialized.payload).toEqual(new DecryptMessageResponsePayload({
+        message: msg
+      }));
+      expect(deserialized.dappOrigin).toEqual(dapp);
+      expect(deserialized.userOrigin).toEqual(account);
+      expect(deserialized.isSuccessful).toEqual(true);
+      expect(deserialized.error).toEqual(RequestError.NoError);
+    });
+
+    it(`e2e - decryptMessage - ${keyType}`, async () => {
+      const msg = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+      const account = new UserAccount(new AccountOption({
+        hasEncryptedPrivateKeyExported: false,
+        keyType: keyType as KeypairType,
+        localKeyEncryptionStrategy: 0
+      }));
+
+      account.unlock(mnemonicToMiniSecret(SEED));
+      await account.init();
+      account.lock();
+
+      // 1. generate an encrypted messaage
+      const encrypted = AsymmetricEncryption.encryptWithCurveType(
+        keyType as 'sr25519' | 'ed25519',
+        msg, account.publicKey
+      );
+
+      // 2. Generate a ephemeral keypair for receiving the data
+      const receivingSK = randomBytes(32);
+      const receivingKey = AsymmetricEncryption.getPublicKey(receivingSK);
+
+      // 3. Build a DecryptMessage Request
+      const request = new DecryptMessageRequest({
+        dappOrigin: new DappDescriptor({
+          activeNetwork: knownNetworks['847e7b7fa160d85f'], // skyekiwi
+          displayName: 'Jest Testing',
+          infoName: 'test',
+          version: 0
+        }),
+        payload: new DecryptMessageRequestPayload({
+          keyType: keyType as KeypairType, message: encrypted, receiptPublicKey: receivingKey
+        }),
+        userOrigin: account
+      });
+
+      expect(request.validatePayload()).toBe(true);
+
+      // 4. Execute the requestHandler
+      const decryptMessage = new DecryptMessageDescriptor();
+
+      account.unlock(mnemonicToMiniSecret(SEED));
+      await account.init();
+      const response = await decryptMessage.requestHandler(request, account);
+
+      // 5. Unwrap with the ephermeral keypair!
+      const m = response.payload.message;
+      const rawM = AsymmetricEncryption.decrypt(receivingSK, m);
+
+      expect(rawM).toEqual(msg);
+    });
+
+    return null;
+  });
+});
