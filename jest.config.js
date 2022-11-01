a5b70f55817e3a0e28430e38ae6d27b5b6122ce6
Song Zhou <song.zhou@skye.kiwi>
2022-11-01 18:30:31 +0800

    use alchemy for assets instead

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ const config = require('@skyekiwi/dev/config/jest.cjs');
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
     '@choko-wallet/abi(.*)$': '<rootDir>/packages/abi/src/$1',
-    '@choko-wallet/assets(.*)$': '<rootDir>/packages/assets/src/$1',
     '@choko-wallet/core(.*)$': '<rootDir>/packages/core/src/$1',
     '@choko-wallet/frontend(.*)$': '<rootDir>/packages/frontend/$1',
     '@choko-wallet/known-networks(.*)$': '<rootDir>/packages/known-networks/src/$1',
@@ -15,7 +14,6 @@ module.exports = Object.assign({}, config, {
   },
   modulePathIgnorePatterns: [
     '<rootDir>/packages/abi/build',
-    '<rootDir>/packages/assets/build',
     '<rootDir>/packages/core/build',
     '<rootDir>/packages/frontend/build',
     '<rootDir>/packages/known-networks/build',
--- packages/known-networks/src/index.ts
+++ packages/known-networks/src/index.ts
@@ -3,20 +3,36 @@
 
 import { KnownNetworks } from '@choko-wallet/core';
 
+import { ArbitrumNetwork, ArbitrumNetworkHash } from './arbitrum';
+import { ArbitrumGoerliNetwork, ArbitrumGoerliNetworkHash } from './arbitrum-goerli';
 import { EthereumNetwork, EthereumNetworkHash } from './ethereum';
 import { GoerliNetwork, GoerliNetworkHash } from './goerli';
 import { KusamaNetwork, KusamaNetworkHash } from './kusama';
+import { OptimismNetwork, OptimismNetworkHash } from './optimism';
+import { OptimismGoerliNetwork, OptimismGoerliNetworkHash } from './optimism-goerli';
 import { PolkadotNetwork, PolkadotNetworkHash } from './polkadot';
+import { PolygonNetwork, PolygonNetworkHash } from './polygon';
+import { PolygonGoerliNetwork, PolygonGoerliNetworkHash } from './polygon-goerli';
+import { PolygonMumbaiNetwork, PolygonMumbaiNetworkHash } from './polygon-mumbai';
 import { RinkebyNetwork, RinkebyNetworkHash } from './rinkeby';
 import { SkyeKiwiNetwork, SkyeKiwiNetworkHash } from './skyekiwi';
 
 /* eslint-disable sort-keys */
-
 export const knownNetworks: KnownNetworks = {
   [SkyeKiwiNetworkHash]: new SkyeKiwiNetwork(),
   [PolkadotNetworkHash]: new PolkadotNetwork(),
   [KusamaNetworkHash]: new KusamaNetwork(),
   [RinkebyNetworkHash]: new RinkebyNetwork(),
   [GoerliNetworkHash]: new GoerliNetwork(),
-  [EthereumNetworkHash]: new EthereumNetwork()
+  [EthereumNetworkHash]: new EthereumNetwork(),
+
+  [PolygonNetworkHash]: new PolygonNetwork(),
+  [PolygonMumbaiNetworkHash]: new PolygonMumbaiNetwork(),
+  [PolygonGoerliNetworkHash]: new PolygonGoerliNetwork(),
+
+  [ArbitrumNetworkHash]: new ArbitrumNetwork(),
+  [ArbitrumGoerliNetworkHash]: new ArbitrumGoerliNetwork(),
+
+  [OptimismNetworkHash]: new OptimismNetwork(),
+  [OptimismGoerliNetworkHash]: new OptimismGoerliNetwork()
 };
