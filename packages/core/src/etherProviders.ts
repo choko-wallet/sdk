9138466af745aba3c031ce6fdc820a7ebf4343cc
Song Zhou <song.zhou@skye.kiwi>
2022-12-19 23:20:09 +0800

    update provider url

--- packages/core/src/etherProviders.ts
+++ packages/core/src/etherProviders.ts
@@ -4,9 +4,9 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 
 export const chainIdToProvider: Record<number, JsonRpcProvider> = {
-  1: new JsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/xhYxjr_vrHG6NrQhCA79tAH9MJ2NOwfc'),
+  1: new JsonRpcProvider('https://mainnet.infura.io/v3/b0125dff016648ed815d5698dfb80b25'),
   137: new JsonRpcProvider('https://polygon-mainnet.g.alchemy.com/v2/mqinV2_-2mL1ZUObw5KNLylyOSkWkIzi'), // polygon
-  5: new JsonRpcProvider('https://eth-goerli.g.alchemy.com/v2/70wjS92mV7V63UCiARGJFJW95dJTldV-', 'goerli'),
+  5: new JsonRpcProvider('https://goerli.infura.io/v3/b0125dff016648ed815d5698dfb80b25', 'goerli'),
   56: new JsonRpcProvider('https://rpc.ankr.com/bsc'),
   80001: new JsonRpcProvider('https://polygon-mumbai.g.alchemy.com/v2/nfLNPHILQKFIhP3HnL-0rI9tpwnaJ7ac'),
   97: new JsonRpcProvider('https://rpc.ankr.com/bsc_testnet_chapel')
