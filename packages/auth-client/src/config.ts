491920eeda1a25ec6742d3ce35cda9b9d513be8b
Song Zhou <song.zhou@skye.kiwi>
2023-03-07 13:35:14 +0800

    update auth server url

--- packages/auth-client/src/config.ts
+++ packages/auth-client/src/config.ts
@@ -1,5 +1,5 @@
 // Copyright 2021-2022 @choko-wallet/auth-client authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export const AUTH_SERVER_URL = 'http://0.0.0.0:8080';
+export const AUTH_SERVER_URL = 'https://auth.choko.app';
 export const KEYGEN_ID_LEN = 32;
