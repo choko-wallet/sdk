2d52468487df3bb6e74e4de2e8acd45ee2f3e298
Song Zhou <song.zhou@skye.kiwi>
2023-03-27 14:49:01 +0800

    allow customized server url for auth-client

--- packages/auth-client/src/oauth.ts
+++ packages/auth-client/src/oauth.ts
@@ -6,18 +6,18 @@ import superagent from 'superagent';
 import { AUTH_SERVER_URL } from './config';
 import { Certificate, OAuthAuthPreimage, OAuthAuthValidate } from './types';
 
-export async function validateOAuthProofOfOwnership (provider: string, email: string, token: string): Promise<Certificate> {
+export async function validateOAuthProofOfOwnership (provider: string, email: string, token: string, url = AUTH_SERVER_URL): Promise<Certificate> {
   const certificateString = await superagent
-    .post(`${AUTH_SERVER_URL}/auth/oauth/validate`)
+    .post(`${url}/auth/oauth/validate`)
     .send({ email, provider, token } as OAuthAuthValidate)
     .accept('json');
 
   return Certificate.fromString(certificateString.text);
 }
 
-export async function preimageOAuthProofOfOwnership (provider: string, email: string): Promise<boolean> {
+export async function preimageOAuthProofOfOwnership (provider: string, email: string, url = AUTH_SERVER_URL): Promise<boolean> {
   const result = await superagent
-    .post(`${AUTH_SERVER_URL}/auth/oauth/preimage`)
+    .post(`${url}/auth/oauth/preimage`)
     .send({ email, provider } as OAuthAuthPreimage)
     .accept('json');
 
