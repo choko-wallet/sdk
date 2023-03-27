7856856e52e0c0114dd43afbb72c49a5f3cd1b14
Song Zhou <song.zhou@skye.kiwi>
2023-03-27 14:46:50 +0800

    add preimage checker for auth-client

--- packages/auth-client/src/oauth.ts
+++ packages/auth-client/src/oauth.ts
@@ -4,7 +4,7 @@
 import superagent from 'superagent';
 
 import { AUTH_SERVER_URL } from './config';
-import { Certificate, OAuthAuthValidate } from './types';
+import { Certificate, OAuthAuthPreimage, OAuthAuthValidate } from './types';
 
 export async function validateOAuthProofOfOwnership (provider: string, email: string, token: string): Promise<Certificate> {
   const certificateString = await superagent
@@ -13,4 +13,19 @@ export async function validateOAuthProofOfOwnership (provider: string, email: st
     .accept('json');
 
   return Certificate.fromString(certificateString.text);
+}
+
+export async function preimageOAuthProofOfOwnership (provider: string, email: string): Promise<boolean> {
+  const result = await superagent
+    .post(`${AUTH_SERVER_URL}/auth/oauth/preimage`)
+    .send({ email, provider } as OAuthAuthPreimage)
+    .accept('json');
+
+  console.log(result.text);
+
+  if (result.text === 'preimage_in_db') {
+    return true;
+  } else {
+    return false;
+  }
 }
