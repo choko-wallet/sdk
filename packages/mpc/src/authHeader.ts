83e8807fbae48c4091807f77c6d7644eb4fe441e
Song Zhou <song.zhou@skye.kiwi>
2023-04-20 08:21:02 +0800

    use restful mpc api

---
+++ packages/mpc/src/authHeader.ts
@@ -0,0 +1,29 @@
+// Copyright 2021-2022 @choko-wallet/app-utils authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { u8aToHex } from '@skyekiwi/util';
+
+import { Certificate } from '@choko-wallet/auth-client/types';
+
+const certificateToAuthHeader = (primary: Certificate, secondary: Certificate, additional?: Certificate): string => {
+  const additionalCert = additional
+    ? {
+      payload: u8aToHex(additional.payload),
+      signature: u8aToHex(additional.signature)
+    }
+    : null;
+
+  return JSON.stringify({
+    additional: additionalCert,
+    primary: {
+      payload: u8aToHex(primary.payload),
+      signature: u8aToHex(primary.signature)
+    },
+    secondary: {
+      payload: u8aToHex(secondary.payload),
+      signature: u8aToHex(secondary.signature)
+    }
+  });
+};
+
+export { certificateToAuthHeader };
