e9a3a52d01ac19f86d2737813eec941a54866a8f
Snyper <snyper@Snypers-MacBook-Pro.local>
2023-05-24 04:59:08 -0400

    fix: unneccessary config delete

--- packages/taro/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/src/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
