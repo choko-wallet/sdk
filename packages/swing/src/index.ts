108671a461849f310c8fcb14b54cf29592b85169
iforcenick <felixsoft.dev@gmail.com>
2023-05-26 01:03:47 +0900

    fix: remove lint issue except any issue

--- packages/swing/src/index.ts
+++ packages/swing/src/index.ts
@@ -1,34 +1,41 @@
-import SwingSDK, { Options, Components, TransferParams, TransferRoute } from '@swing.xyz/sdk'
-import EventEmitter from 'events'
+// Copyright 2021-2022 @choko-wallet/swing authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import SwingSDK, { Components, Options, TransferParams, TransferRoute } from '@swing.xyz/sdk';
+import EventEmitter from 'events';
 
 export const initSwing = async (options?: Options): Promise<{
   sdk: SwingSDK
   eventEmitter: EventEmitter
 }> => {
-  const sdk = new SwingSDK(options)
-  await sdk.init()
-  const eventEmitter = new EventEmitter()
-  sdk.on('TRANSFER', status => eventEmitter.emit('TRANSFER', status))
-  sdk.on('READY', () => eventEmitter.emit('READY'))
+  const sdk = new SwingSDK(options);
+
+  await sdk.init();
+  const eventEmitter = new EventEmitter();
+
+  sdk.on('TRANSFER', (status) => eventEmitter.emit('TRANSFER', status));
+  sdk.on('READY', () => eventEmitter.emit('READY'));
+
   return {
-    sdk,
-    eventEmitter
-  }
-}
+    eventEmitter,
+    sdk
+  };
+};
 
-export const connectWallet = async (sdk: SwingSDK, provider: any, chainSlug: Components.Schemas.ChainSlug) => {
-  await sdk.wallet.connect(provider, chainSlug)
-}
+export const connectWallet = async (sdk: SwingSDK, provider: any, chainSlug: Components.Schemas.ChainSlug): void => {
+  await sdk.wallet.connect(provider, chainSlug);
+};
 
-export const getQuote = async (sdk: SwingSDK, transferParams: TransferParams) => {
-  const quote = await sdk.getQuote(transferParams)
-  return quote
-}
+export const getQuote = async (sdk: SwingSDK, transferParams: TransferParams): any => {
+  const quote = await sdk.getQuote(transferParams);
 
-export const transfer = async (sdk: SwingSDK, transferParams: TransferParams, transferRoute: TransferRoute) => {
-  await sdk.transfer(transferRoute, transferParams)
-}
+  return quote;
+};
 
-export const getTransactionHistory = async (sdk: SwingSDK, address: string) => {
-  return await sdk.wallet.getTransactions(address)
-}
+export const transfer = async (sdk: SwingSDK, transferParams: TransferParams, transferRoute: TransferRoute): void => {
+  await sdk.transfer(transferRoute, transferParams);
+};
+
+export const getTransactionHistory = async (sdk: SwingSDK, address: string): any => {
+  return await sdk.wallet.getTransactions(address);
+};
